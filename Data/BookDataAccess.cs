40dccd7da65a9987e51b58f49b155d326484980f
camilogs <camilo.gonzalez.col@gmail.com>
2023-04-29 19:44:22 -0500

    Add GetBooksByCategory method

--- Data/BookDataAccess.cs
+++ Data/BookDataAccess.cs
@@ -30,4 +30,22 @@ public class BookDataAccess
     {
         return _books.Where(b => b.PageCount > 200 && b.Tittle.ToLower().Contains("in action"));
     }
+
+    public bool AllBooksHasState()
+    {
+        return _books.All(b => !String.IsNullOrEmpty(b.Status));
+    }
+
+    public bool AnyBookWasPublishedIn2005()
+    {
+        return _books.Any(b => b.PublishedDate.Year == 2005);
+    }
+
+    public IEnumerable<BookModel> GetBooksByCategory(string categoryBook)
+    {
+        return _books.Where(b => b.Categories.Contains(categoryBook));
+    }
+    
+
+
 }
--- Program.cs
+++ Program.cs
@@ -5,11 +5,15 @@ var bookDataAccess = new BookDataAccess();
 var AllBooks = bookDataAccess.GetAllBooks();
 var BooksAfter2000 = bookDataAccess.GetBooksPublishedAfterYear(2000);
 var InActionsMore200Pages = bookDataAccess.GetBooksWithMoreThan200pagesAndContainsInAction();
+var PythonBooks = bookDataAccess.GetBooksByCategory("Python");
 
 
 var animalDataAccess = new AnimalDataAccess();
 var GreenAnimalWithStartWithVowel = animalDataAccess.GetGreenAnimalsWhichNameStartWithVowel();
 
+
 var consoleUI = new ConsoleUI();
-consoleUI.PrintAnimals(GreenAnimalWithStartWithVowel);
-//consoleUI.PrintBooks(AllBooks);
+
+consoleUI.PrintBooks(PythonBooks);
+Console.WriteLine(bookDataAccess.AnyBookWasPublishedIn2005());
+//consoleUI.PrintAnimals(GreenAnimalWithStartWithVowel);
